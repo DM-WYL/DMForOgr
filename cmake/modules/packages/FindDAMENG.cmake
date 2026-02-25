d809d5f5ed92f5ff301589f04ff01f6dd0c846c5
DM-WYL <wuyilun@dameng.com>
2026-02-25 14:54:46 +0800

    Try to fix CMakeError in actions run.

--- cmake/modules/packages/FindDAMENG.cmake
+++ cmake/modules/packages/FindDAMENG.cmake
@@ -1,10 +1,7 @@
 set(DM_HOME $ENV{DM_HOME})
-if(NOT DM_HOME)
-    message(STATUS "needs set DM_HOME for ogr_DAMENG enable")
-endif()
 
-find_path(DAMENG_INCLUDE_DIRS NAMES DPI.h PATHS
-    "${DM_HOME}/include/include"
+find_path(DAMENG_INCLUDE_DIR NAMES DPI.h PATHS
+    "${DM_HOME}/include"
     NO_DEFAULT_PATH
 )
 find_library(DAMENG_LIBRARY NAMES libdmdpi dmdpi PATHS
@@ -14,5 +11,16 @@ find_library(DAMENG_LIBRARY NAMES libdmdpi dmdpi PATHS
     NO_DEFAULT_PATH
 )
 
-set(DAMENG_LIBRARIES ${DAMENG_LIBRARY})
-set(DAMENG_INCLUDE_DIRS ${DAMENG_INCLUDE_DIRS})
+include(FindPackageHandleStandardArgs)
+if(DAMENG_LIBRARY AND DAMENG_INCLUDE_DIR)
+    set(DAMENG_FOUND TRUE)
+    set(DAMENG_LIBRARIES ${DAMENG_LIBRARY})
+    set(DAMENG_INCLUDE_DIRS ${DAMENG_INCLUDE_DIR})
+    mark_as_advanced(DAMENG_INCLUDE_DIR DAMENG_LIBRARY)
+    message(STATUS "Found Dameng: ${DAMENG_LIBRARY}")
+else()
+    set(DAMENG_FOUND FALSE)
+    set(DAMENG_LIBRARIES "")
+    set(DAMENG_INCLUDE_DIRS "")
+    message(STATUS "Dameng not found (set DM_HOME to enable Dameng support)")
+endif()
